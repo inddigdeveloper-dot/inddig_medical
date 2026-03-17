4d07c37f21b8b24d462bccc623c553cee1daaff5
inddigdeveloper-dot <inddigdeveloper@gmail.com>
2026-03-17 14:25:27 +0530

    added files in github

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/inddig-medical/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
